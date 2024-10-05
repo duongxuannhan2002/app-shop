ba7e4ce0fa64953eff36eb99fdda374d24c4217a
Phung Phu <phuphung123789@gmail.com>
2024-10-05 12:27:52 +0700

    phu

--- components/Content/HomeScreen/index.js
+++ components/Content/HomeScreen/index.js
@@ -3,7 +3,9 @@ import { Text, View } from "react-native"
 export default function HomeScreen(){
     return(
         <View>
-            <Text>Đây là HomeScreen</Text>
+            <Text>Đây là HomeScreen
+                có con cá con
+            </Text>
         </View>
     )
 }
