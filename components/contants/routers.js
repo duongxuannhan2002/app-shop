7d5fb0884309dc5893c468f19e774f8a83ad9392
Phung Phu <phuphung123789@gmail.com>
2024-10-05 12:49:57 +0700

    phu2

---
+++ components/contants/routers.js
@@ -0,0 +1,8 @@
+export const ADMIN_PATH = "/admin";
+export const ADMIN_USERS_PATH = "users";
+
+export const HOME_PATH = "/";
+export const SIGNUP_PATH = "signup";
+export const SIGNIN_PATH = "signin";
+export const HISTORY_PATH = "history";
+export const CONTACT = "/contact";
