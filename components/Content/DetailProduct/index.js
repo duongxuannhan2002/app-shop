516082e212718c4a4936e0c07aa2721df47fdd09
duongxuannhan2002 <sieuxaya37.na@gmail.com>
2024-10-16 14:01:30 +0700

    cccccvv

--- components/Content/DetailProduct/index.js
+++ components/Content/DetailProduct/index.js
@@ -1,3 +1,4 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
 import React, { useState, useEffect } from 'react';
 import {
--- components/Content/Header/index.js
+++ components/Content/Header/index.js
@@ -1,18 +1,45 @@
-import React from 'react';
-import { View, TextInput, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, StyleSheet, Image, TouchableOpacity, FlatList, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons'; // Thay đổi nếu bạn sử dụng thư viện icon khác
 import { useNavigation } from '@react-navigation/native';
 
 const Header = () => {
   const navigation = useNavigation();
+  const [searchKeyword, setSearchKeyword] = useState(''); // Từ khóa tìm kiếm
+  const [searchResults, setSearchResults] = useState([]); // Kết quả tìm kiếm
+
+  const fetchProducts = (keyword) => {
+    // Dữ liệu giả lập từ API
+    fetch(`https://ttcs-delta.vercel.app/api/v1/get-shoes-by-search?key=${keyword}`) // Gọi API
+      .then((response) => response.json()) // Chuyển đổi dữ liệu nhận được thành JSON
+      .then((json) => {
+        setSearchResults(json.data)
+        // Lưu dữ liệu vào state
+      })
+      .catch((error) => {
+        console.error(error); // Bắt lỗi nếu có
+      });
+  };
+
+  // Hàm xử lý tìm kiếm khi từ khóa thay đổi
+  const handleSearch = (keyword) => {
+    setSearchKeyword(keyword); // Cập nhật từ khóa
+    fetchProducts(keyword); // Gọi API với từ khóa
+  };
+
   return (
     <View style={styles.container}>
       {/* Phần trên cùng với thanh tìm kiếm */}
       <View style={styles.topSection}>
         <View style={styles.searchBar}>
-          <TextInput placeholder="Search" style={styles.searchInput} />
+          <TextInput
+            placeholder="Search"
+            style={styles.searchInput}
+            value={searchKeyword}
+            onChangeText={handleSearch} // Gọi hàm tìm kiếm mỗi khi từ khóa thay đổi
+          />
         </View>
-        <Icon name="person-outline" size={30} style={styles.profileIcon} onPress = {() => navigation.navigate('userinfo')}/>
+        <Icon name="person-outline" size={30} style={styles.profileIcon} onPress={() => navigation.navigate('userinfo')} />
       </View>
 
       {/* Phần dưới với các icon */}
@@ -22,7 +49,7 @@ const Header = () => {
           <Icon name="home-outline" size={30} style={styles.icon} />
         </TouchableOpacity>
 
-        {/* Thay thế icon bằng hình ảnh (Profile) */}
+        {/* Thay thế icon bằng hình ảnh (Product) */}
         <TouchableOpacity onPress={() => navigation.navigate('Product')}>
           <Image
             source={require('../../../assets/image/shoe-icon.png')}
@@ -35,6 +62,18 @@ const Header = () => {
           <Icon name="cart-outline" size={30} style={styles.icon} />
         </TouchableOpacity>
       </View>
+
+      {/* Hiển thị danh sách sản phẩm */}
+      <FlatList
+        data={searchResults}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <TouchableOpacity style={styles.productItem} onPress={() => navigation.navigate('ProductDetails', { product: item.id })}>
+            <Image source={{ uri: item.image }} style={styles.productImage} />
+            <Text style={styles.productName}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 };
@@ -81,6 +120,19 @@ const styles = StyleSheet.create({
     width: 30, // Chiều rộng của ảnh
     height: 30, // Chiều cao của ảnh
     marginHorizontal: 20,
+  },
+  productItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+  },
+  productImage: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  productName: {
+    fontSize: 16,
   },
 });
 
